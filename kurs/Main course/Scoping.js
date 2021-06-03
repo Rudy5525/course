88b11b60018b00c32ebabab7ffefc9faa3a60675
Rudy5525 <laljacek@gmail.com>
2021-06-03 20:40:55 +0200

    Behind the scenes

---
+++ kurs/Main course/Scoping.js
@@ -0,0 +1,36 @@
+'use strict';
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, you're a millenial ${firstName}.`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT';
+    }
+    console.log(output);
+    // console.log(str);    // Refference error (str is block scoped in if statement)
+    console.log(millenial); // No error (millenial is declared by var, so it ignores block scoping and is function scoped)
+    // add(2, 3); // Reference error (add() is block scoped in if statement)
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);    // Refference error (age variable is function scoped in calcAge function)
+// printAge();          // Refference error (printAge() is function scoped in calcAge())
+*/
